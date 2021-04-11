0f5789719331e974ba3d784f8581aa8b0f0bac6a
Ludovic DEPLANQUE <ldeplanque@portdedunkerque.fr>
2021-04-11 12:42:14 +0200

    Ajout d'une option X2 dans la fenêtre de rendu (mode édition)

--- Interfaces/Rendu.Designer.cs
+++ Interfaces/Rendu.Designer.cs
@@ -24,6 +24,7 @@
 		/// </summary>
 		private void InitializeComponent() {
 			this.pictureBox = new System.Windows.Forms.PictureBox();
+			this.chkX2 = new System.Windows.Forms.CheckBox();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -32,15 +33,29 @@
 			this.pictureBox.Location = new System.Drawing.Point(0, 0);
 			this.pictureBox.Name = "pictureBox";
 			this.pictureBox.Size = new System.Drawing.Size(768, 544);
+			this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox.TabIndex = 0;
 			this.pictureBox.TabStop = false;
+			// 
+			// chkX2
+			// 
+			this.chkX2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+			this.chkX2.AutoSize = true;
+			this.chkX2.Location = new System.Drawing.Point(774, 240);
+			this.chkX2.Name = "chkX2";
+			this.chkX2.Size = new System.Drawing.Size(37, 17);
+			this.chkX2.TabIndex = 1;
+			this.chkX2.Text = "x2";
+			this.chkX2.UseVisualStyleBackColor = true;
+			this.chkX2.CheckedChanged += new System.EventHandler(this.chkX2_CheckedChanged);
 			// 
 			// Rendu
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(769, 544);
+			this.ClientSize = new System.Drawing.Size(809, 544);
 			this.ControlBox = false;
+			this.Controls.Add(this.chkX2);
 			this.Controls.Add(this.pictureBox);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.MaximizeBox = false;
@@ -51,12 +66,14 @@
 			this.TopMost = true;
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.PictureBox pictureBox;
+		private System.Windows.Forms.CheckBox chkX2;
 
 	}
 }
--- Interfaces/Rendu.cs
+++ Interfaces/Rendu.cs
@@ -9,5 +9,21 @@ namespace ConvImgCpc {
 			InitializeComponent();
 			pictureBox.Image = b;
 		}
+
+		private void chkX2_CheckedChanged(object sender, System.EventArgs e) {
+			if (chkX2.Checked) {
+				this.Width = 1593;
+				this.Height = 1127;
+				pictureBox.Width = 1536;
+				pictureBox.Height = 1088;
+			}
+			else {
+				this.Width = 825;
+				this.Height = 583;
+				pictureBox.Width = 768;
+				pictureBox.Height = 544;
+			}
+			pictureBox.Refresh();
+		}
 	}
 }
